429e536e57d4e5a4d7a7eaa8d451a58093537439
Jaro Marval <jampgold@gmail.com>
2015-05-24 03:09:04 -0430

    Correciones y puesta en marcha de la publicación en Redes Sociales

--- diad/models.py
+++ diad/models.py
@@ -4,6 +4,7 @@ from PIL import Image
 from django.conf import settings
 from django.db import models
 from django.db.models.signals import post_save
+from datetime import datetime
 from twython import Twython
 from facebook import GraphAPI
 
@@ -12,22 +13,23 @@ descripcion = 'Nueva imagen subida a http://uniforme.scoutsfalcon.org, Celebrand
 # Create your models here.
 class Album(models.Model):
     album = models.IntegerField()
-    url = models.ImageField(upload_to=settings.APP_NAME+'/static/img/portada/%Y')
+    url = models.ImageField(upload_to=settings.APP_NAME+'/static/img/portada')
     creado_at = models.DateTimeField(auto_now_add=True)
 
 class Fotos(models.Model):
-    url = models.ImageField(upload_to=settings.APP_NAME+'/static/img/album/%Y')
     album = models.IntegerField()
+    url = models.ImageField(upload_to=settings.APP_NAME+'/static/img/album/%Y')
     autorizado = models.BooleanField(default=False)
     creado_at = models.DateTimeField(auto_now_add=True)
 
 def Redimensionar(uri):
+    uri = os.path.join(settings.BASE_DIR, uri)
     size = 450, 350
 
     try:
-        im = Image.open(URI)
+        im = Image.open(uri)
         im.thumbnail(size, Image.ANTIALIAS)
-        im.save(URI, "JPEG")
+        im.save(uri, "JPEG")
     except IOError:
         pass
 
@@ -35,34 +37,34 @@ def PrepararURL(instancia):
     if str(instancia.url).find(settings.APP_NAME) == -1:
         pass
     else:
-        Redimensionar(instancia.url)
         instancia.url = str(instancia.url).replace(settings.APP_NAME, '')
         instancia.save()
 
 def PostAlbum(sender, instance, **kwargs):
-    uri = os.path.join(settings.BASE_DIR, str(instance.url))
-    Redimensionar(uri)
+    url = str(instance.url)
+    Redimensionar(url)
     PrepararURL(instance)
 
 def PostFotos(sender, instance, **kwargs):
-    uri = os.path.join(settings.BASE_DIR, str(instance.url))
+    url = str(instance.url)
 
-    Redimensionar(uri)
+    Redimensionar(url)
     PrepararURL(instance)
 
-    if instance.autorizado:
+    if instance.autorizado and instance.album == datetime.now().year:
+        uri = os.path.join(settings.BASE_DIR,settings.APP_NAME)+url
         PublicarTwitter(uri)
         PublicarFacebook(uri)
+
 
 def PublicarTwitter(uri):
     try:
         twitter = Twython(settings.TW_KEY, settings.TW_SECRET, settings.TW_TOKEN, settings.TW_TOKEN_SECRET)
 
         image_id = twitter.upload_media(media=open(uri, 'rb'))
-        twitter.update_status(status=descripcion+' // @ScoutsFalcon @scoutsvenezuela', media_ids=image_id['media_id'])
+        twitter.update_status(status=descripcion, media_ids=image_id['media_id'])
     except Exception, e:
         logging.error(e)
-
 
 def PublicarFacebook(uri):
     try:

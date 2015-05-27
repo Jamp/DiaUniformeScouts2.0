6b318dd53bff4dde8ba5a9688c1a5d731d473933
Jaro Marval <jampgold@gmail.com>
2015-05-26 23:51:06 -0430

    Error en la fecha del día del uniforme

--- diad/static/js/countdown.js
+++ diad/static/js/countdown.js
@@ -6,7 +6,7 @@ COMMING SOON PAGE
     * Set your date here  (YEAR, MONTH (0 for January/11 for December), DAY, HOUR, MINUTE, SECOND)
     * according to the GMT+0 Timezone
     **/
-    var launch = new Date(2015, 04, 28, 04, 00);
+    var launch = new Date(2015, 04, 27, 00, 00);
     /**
     * The script
     **/

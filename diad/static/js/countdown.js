74d495820647d34c3f26be231bdb9a2bf0f84c7c
Jaro Marval <jampgold@gmail.com>
2017-05-23 12:01:33 -0400

    Corrigiendo el detalle del contador regresivo

--- diad/static/js/countdown.js
+++ diad/static/js/countdown.js
@@ -6,7 +6,7 @@ COMMING SOON PAGE
     * Set your date here  (YEAR, MONTH (0 for January/11 for December), DAY, HOUR, MINUTE, SECOND)
     * according to the GMT+0 Timezone
     **/
-    var launch = new Date(2017, 04, 24, 00, 00);
+    var launch = new Date(2017, 04, 24, 04, 00);
     /**
     * The script
     **/
